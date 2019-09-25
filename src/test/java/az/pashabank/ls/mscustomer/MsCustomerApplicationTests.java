c58eeed2e992a491f0bd8fcc12e16ec8bd66c98b
NigarMovsumova <nigar.movsumova@pashabank.az>
2019-09-25 13:38:52 +0400

    Initial commit

---
+++ src/test/java/az/pashabank/ls/mscustomer/MsCustomerApplicationTests.java
@@ -0,0 +1,16 @@
+package az.pashabank.ls.mscustomer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsCustomerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
